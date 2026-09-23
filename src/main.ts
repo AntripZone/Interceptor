02f58a71bdd884ac727eb0da61637ec7789715fa
AdrianAlvaT <adrian_alvat@hotmail.com>
2026-09-23 15:19:50 -0500

    fix: Estudiante 4

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { configureApp } from './app.setup.js';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   configureApp(app);
+
+  const config = new DocumentBuilder()
+    .setTitle('Clínica Integral API')
+    .setDescription('API de gestión de pacientes y médicos')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
